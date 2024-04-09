31ab7ab64e3fb53b3002cbef08a86d5be2b9b437
angel.garcia <angel.garcia@students.salle.url.edu>
2024-04-09 17:17:19 +0200

    Started moduling

---
+++ P1/Practica1_Periferics/src/globals.h
@@ -0,0 +1,38 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include "stm32f4xx.h"
+#include "stm32f429i_discovery.h"
+
+#define MAX_BOUNCES_MS 300
+
+int counter_led = 0;
+//unsigned long num_overflows_right = 0;
+//unsigned long num_overflows_left = 0;
+unsigned int Wd = 0; // Right speed
+unsigned int We = 0; // Left speed
+uint32_t micros_period_right = 0;
+uint32_t micros_period_left = 0;
+
+int vehicle_speed = 45;//TODO
+int vehicle_speed_ascendent = 1; //1 = ascendent, 0 = descendent
+int i = 0;
+
+int difference_factor_ascendent = 1; //1 = ascendent, 0 = descendent
+int num_cycles = 0; //Number of times that we have arrived to/started from the initial speed difference factor (1)
+int calculating_right_speed = 0; //1 = calculating right speed, 0 = calculating left speed
+float right_wheel_speed = 0;
+float left_wheel_speed = 0;
+int j = 0;
+
+int signal1_microseconds = 0;
+int signal2_microseconds = 0;
+
+int right_wheel_signal_period_us;
+int left_wheel_signal_period_us;
+
+int ms_since_edge_rising = 0;
+int bouncing_counter_ms = 0;
+int last_bouncing_counter_ms = 0;
+
+#endif
---
+++ P1/Practica1_Periferics/src/timers.h
@@ -0,0 +1,10 @@
+#ifndef TIMERS_H
+#define TIMERS_H
+
+#include "globals.h"
+
+void init_TIM2(void); // 1ms
+void init_TIM3(void); // 2us
+void init_TIM5(void); // 50us
+
+#endif
